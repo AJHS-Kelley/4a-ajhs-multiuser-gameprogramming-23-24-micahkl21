6d5aa9c6df5dfc35d5c84a5ccc3d49507d57ab29
micahkl21 <111576504+micahkl21@users.noreply.github.com>
2023-10-05 14:26:42 -0400

    v0.1

--- multiUserGameProgramming/computer_science_exercises/02_Collections/Program.cs
+++ multiUserGameProgramming/computer_science_exercises/02_Collections/Program.cs
@@ -1,4 +1,4 @@
-﻿// 02_Collections, Lecount Micah, 10-5-23, v0.0
+﻿// 02_Collections, Lecount Micah, 10-5-23, v0.1
 using Systems;
 
 namespace _02_collections
@@ -7,6 +7,21 @@ namespace _02_collections
     {
         static void Main(string[] args)
         {
+            // Collections are variables that can store multiple values in one variable.
+
+            /* Arrays
+            -- Number of elements in an array CANNOT change. 
+            -- Contents of elements in an array CAN change
+            -- Items in the array are called ELEMTS.
+            -- Arrays are ordered, meaning each item has a fixed postion 
+            -- The position is known as the INDEX.
+            -- first element in an array is index 0.
+            */
+
+            // Declaring and Defining an Array
+            string[] breakfastFoods = ("Bacon", "Waffles", "Pancakes", "Cereal", "Parfait");
+            int[] testScores = (95, 100, 25, 15, 27, 35);
+            float[] GPA = (3.14f, 2.25f, 1.74f, 1.99f, 099f, 4.25f);
             
         }
     }
