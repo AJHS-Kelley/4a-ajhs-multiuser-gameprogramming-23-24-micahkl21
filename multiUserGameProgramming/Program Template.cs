fb527d9fdf6f52f7ad67931b3a2397e58d9ac393
micahkl21 <111576504+micahkl21@users.noreply.github.com>
2023-11-09 14:19:06 -0500

    v0.2

--- multiUserGameProgramming/Program Template.cs
+++ multiUserGameProgramming/Program Template.cs
@@ -1,13 +1,40 @@
-// Micah Lecouny, Program Template, v0.4
-using System;
-
-namespace programTemplate
-{                                                                                                                                                                                                                                             
-    class UPDATEFOREACHPROGRAM  
+// Lecount Micah, Program Template, v0.2
+using System; 
+/*
+Void Method
+Method with Return
+Method with Parameter
+Method with Multiple Parameters
+Method with Default Parameters
+Method Class using Named Arguments
+Method Overloading
+*/
+namespace MethodsParameters
+{
+        class MethodsParameters
     {
-        static void Main(string[] args)
+        // METHOD - - named block of code, can be used over and over.
+        // All methods have a SIGNATURE that defines their name, parameters, and output.
+        // Example Signature
+        static void MyMethod()
         {
-
+         Console .WriteLine("I like mine with lettuce and tomatoes, Heinz 57, and french fries")
         }
-    }
+        // static -- This method belongs to the current class, it is NOT an object.
+        // void -- This method has no return value.
+}
+        static int DoubleUp()
+        {
+            int sum = 0;
+            Console.WriteLine("This method will double a number and return it.\n");
+            Console.WriteLine("Please enter a number on the next line.\n");
+            sum = System. Convert. ToInt32(Console. ReadLine ()) ;
+            sum *= 2;
+            Console.WriteLine (sum):
+            return sum;
+        }
+
+{
+
+}
 }
